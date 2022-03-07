7ebd05b9cb1a078893ab4ab90d85ffa72929471a
Andre <andreferreira@live.co.uk>
2022-03-07 23:19:23 +0000

    changes to main TS file - new request

--- apps/urlcreation/src/main.ts
+++ apps/urlcreation/src/main.ts
@@ -4,12 +4,19 @@
  */
 
 import * as express from 'express';
+import { Request, Response} from "express";
+import * as randomstring from "randomstring";
 
 const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to this new app!' });
 });
+
+app.get("/api/shorturl", (req: Request, res: Response) => {
+  const generateRandomString = randomstring.generate(6);
+  return res.json({ url: `https://localhost:3333/api/${generateRandomString}`})
+})
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
