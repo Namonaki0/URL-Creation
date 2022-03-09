a88962d6bc176dabe35cc7f019ab7e426ddc2014
Andre <andreferreira@live.co.uk>
2022-03-09 23:08:39 +0000

    connecting to MongoDB

--- apps/urlcreation/src/main.ts
+++ apps/urlcreation/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 import { Request, Response} from "express";
 import * as randomstring from "randomstring";
@@ -10,22 +5,32 @@ import { MongoClient } from "mongodb";
 
 const app = express();
 
-// const url = ``;
-// const client = new MongoClient(url);
+//? CONNECTION TO CLUSTER
+const uri = "mongodb+srv://cluster1.qabkv.mongodb.net/urlcreation";
+const client = new MongoClient(uri);
 // const dbName = "urlcreation";
 
+//? DB CONNECTION FUNCTION
+const connectToDb = async() => {
+  try {
+    await client.connect();
+    console.log("Connected to DB");
+    // const db = client.db(dbName);
+    // const collections = db.collection("urls");
+  } 
+  catch(err) {
+      console.error(err)
+  }
+  finally {
+    await client.close();
+  }
+}
+//? DB CONNECTION FUNCTION --- END
 
-// const connectToDb = async() => {
-//   await client.connect();
-//   console.log("Connected to DB");
-//   const db = client.db(dbName);
-//   const collections = db.collection("urls");
-// }
+connectToDb()
+  .catch(console.error)
 
-// connectToDb()
-//   .then(console.log)
-//   .catch(console.error)
-//   .finally(() => client.close())
+
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to this new app!' });
